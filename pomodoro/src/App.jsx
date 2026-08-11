f796a67cc2cb111865da9b4e3b8dd53d638ed28e
Cristóbal Ulloa <cristobalu2006@gmail.com>
2026-08-11 02:02:03 -0400

    Animación del Corazón agregada, mejora de estructura de flexbox.

--- pomodoro/src/App.jsx
+++ pomodoro/src/App.jsx
@@ -1,27 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-import corazon from './assets/corazon.png'
+import Corazonanim from './components/corazon'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
       <main>
-        <div className='divtitulo'>
-          <h1>UN VERANO ESTUDIANDO</h1>
-          <p>v.0.0.1</p>
-        </div>
-        <div className='boxContador'>
-          <h1>5:00</h1>
+        <div className='box'>
+          <div className='boxleft'>
+            <div className='divtitulo'>
+              <h1>UN VERANO ESTUDIANDO</h1>
+              <p>v.0.0.1</p>
+            </div>
+            
+            <div className='boxDibujo'>
+              <Corazonanim/>
+            </div>
+          </div>
+
+          <div className='boxright'>
+
+          <div className='boxContador'>
+            <h1>5:00</h1>
           <progress value={0} max={100} className='progressBar'></progress>
+          <div className='boxBotones'>
+            <button className='empezarButton'>Empezar</button>
+            <button className='checkPointButton'>Checkpoint</button>
+          </div>
+          </div>
+          </div>
+
+
+
+
         </div>
-        <div className='boxBotones'>
-          <button className='empezarButton'>Empezar</button>
-          <button className='checkPointButton'>Checkpoint</button>
-        </div>
-        <div className='boxDibujo'>
-          <img src={corazon} className='corazon' />
-        </div>
+
+
       </main>
     <footer>
       <div>
---
+++ pomodoro/src/components/corazon.jsx
@@ -0,0 +1,24 @@
+import { useState,useEffect} from 'react'
+
+import corazon from '../assets/corazon.png'
+import corazon1 from '../assets/corazon2.png'
+
+
+
+function Corazonanim() {
+    const frames = [corazon, corazon1]
+    const  [frame, setFrame] = useState(0)
+
+    useEffect(()=> {
+        const intervalo = setInterval(() => {
+            setFrame(frame => (frame+1) % frames.length)
+        }, 1000)
+
+        return () => clearInterval(intervalo)
+    }, [])
+    return (
+        <img src={frames[frame]} alt="" className='corazon' />
+    )
+}
+
+export default Corazonanim
